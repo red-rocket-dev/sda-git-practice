6e9b0315eb659a1e959c818716fba80f484f4bdb
Jakub <kubplon@gmail.com>
2020-06-07 10:51:50 +0200

    ask for name

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,8 +1,11 @@
 package pl.sda;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("asdasd");
-        System.out.println("test test");
+        System.out.println("Podaj imie");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
     }
 }
