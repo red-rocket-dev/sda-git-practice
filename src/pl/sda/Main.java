576d48faf79f41d60c8a0515bcb47c7c2671e504
Jakub <kubplon@gmail.com>
2020-06-07 10:29:58 +0200

    3 x "test test"

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("asdasd");
         System.out.println("test test");
+        System.out.println("test test");
+        System.out.println("test test");
+        System.out.println("test test");
     }
 }
