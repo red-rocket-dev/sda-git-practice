863cdce0f0f8e7b703fc097e9b65074f1d5e0080
Jakub <kubplon@gmail.com>
2020-06-07 11:06:03 +0200

    some new commit

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //TODO: gitflow
         System.out.println("Podaj imie");
+        System.out.println("aaaa");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         printName(name);
