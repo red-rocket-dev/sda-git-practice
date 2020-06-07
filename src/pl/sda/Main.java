14bbb5c036368863ffe6ec208f6471fbd5275e2a
Jakub <kubplon@gmail.com>
2020-06-07 10:53:25 +0200

    print name

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -5,4 +5,8 @@ public class Main {
         System.out.println("asdasd");
         System.out.println("test test");
     }
+
+    private static void printName(String name) {
+        System.out.println("Twoje imie to " + name);
+    }
 }
